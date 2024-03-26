a26cd1a2e19d1420f41b2baa8e5bc0d103affe8c
Gagasbandas <kjjcds@gmail.com>
2024-03-26 15:47:20 +0900

    expert_policy 측정

--- aai4160/scripts/log_expert_policy.py
+++ aai4160/scripts/log_expert_policy.py
@@ -1,6 +1,12 @@
 import os
 import time
 import argparse
+import pickle
+from collections import OrderedDict
+
+import gym
+import numpy as np
+import torch
 
 from aai4160.infrastructure.bc_trainer import BCTrainer
 from aai4160.agents.bc_agent import BCAgent
@@ -8,11 +14,12 @@ from aai4160.policies.loaded_gaussian_policy import LoadedGaussianPolicy
 from aai4160.infrastructure.utils import MJ_ENV_KWARGS, MJ_ENV_NAMES
 
 from aai4160.infrastructure import pytorch_util as ptu
+from aai4160.infrastructure import utils
+from aai4160.infrastructure.logger import Logger
 
 def parseArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument("--expert_policy_file", type=str, default="aai4160/policies/experts/Ant.pkl")
-    parser.add_argument('--expert_data', '-ed', type=str, required=True)
     parser.add_argument('--env_name', '-env', type=str,
         help=f'choices: {", ".join(MJ_ENV_NAMES)}', required=True)
     parser.add_argument('--ep_len', type=int)
@@ -30,14 +37,59 @@ if __name__ == "__main__":
     # load expert policy
     args = parseArgs()
 
+    seed = args.seed
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    ptu.init_gpu(
+        use_gpu=not args.no_gpu,
+        gpu_id=args.which_gpu
+    )
+
     ## directory for logging
     data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
         '../../data')
     if not os.path.exists(data_path):
         os.makedirs(data_path)
-    logdir = 'ex' + args.exp_name + '_' + args.env_name + '_' + \
+    logdir = 'ex_' + args.env_name + '_' + \
         time.strftime("%d-%m-%Y_%H-%M-%S")
     logdir = os.path.join(data_path, logdir)
     if not os.path.exists(logdir):
         os.makedirs(logdir)
+
+    logger = Logger(logdir)
+    env = gym.make(args.env_name, **MJ_ENV_KWARGS[args.env_name])
+    env.reset(seed=args.seed)
+
+    print('Loading expert policy from...', args.expert_policy_file)
+    loaded_expert_policy = LoadedGaussianPolicy(
+        args.expert_policy_file).to('cpu')
+    print('Done restoring expert policy...')
+
+    # collect eval trajectories, for logging
+    print("\nCollecting data for eval...")
+    eval_paths, eval_envsteps_this_batch = utils.sample_trajectories(
+        env, loaded_expert_policy, args.eval_batch_size, args.ep_len)
+
+    eval_returns = [eval_path["reward"].sum() for eval_path in eval_paths]
+    eval_ep_lens = [len(eval_path["reward"]) for eval_path in eval_paths]
+
+    print("\nEval data collected:")
+    logs = OrderedDict()
+
+    logs["Eval_AverageReturn"] = np.mean(eval_returns)
+    logs["Eval_StdReturn"] = np.std(eval_returns)
+    logs["Eval_MaxReturn"] = np.max(eval_returns)
+    logs["Eval_MinReturn"] = np.min(eval_returns)
+    logs["Eval_AverageEpLen"] = np.mean(eval_ep_lens)
+
+    for key, value in logs.items():
+        print('{} : {}'.format(key, value))
+        logger.log_scalar(value, key, 0)
+
+    logger.flush()
+
+    print("Done!")
+
+
+
 
--- draw_graph.py
+++ draw_graph.py
@@ -1,12 +1,21 @@
 import matplotlib
 import matplotlib.pyplot as plt
+#
+# x = [1000, 4000, 7000, 10000]
+# y = [3267, 4459, 4763, 4794]
+#
+# plt.xlabel('num_agent_train_steps_per_iter')
+# plt.ylabel('eval average reward')
+# plt.title('Ant-v4')
+# plt.xticks(x)
+# plt.plot(x, y, label='Ant', color='b', marker='o', linestyle='-')
+# plt.show()
 
-x = [1000, 4000, 7000, 10000]
-y = [3267, 4459, 4763, 4794]
+x = ['Ant', 'Walker2d']
+y = [4788, 5163]
 
-plt.xlabel('num_agent_train_steps_per_iter')
+plt.xlabel('env_name')
 plt.ylabel('eval average reward')
-plt.title('Ant-v4')
-plt.xticks(x)
-plt.plot(x, y, label='Ant', color='b', marker='o', linestyle='-')
+
+plt.bar(x, y, color='b')
 plt.show()
